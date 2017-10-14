bd3699a564775292116e6e2aa0a248cd95d6417d
idisin91 <idisin91@gmail.com>
2017-10-14 13:01:57 +0300

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Никита Скрынников on 14.10.17.
+//  Copyright © 2017 ForMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
